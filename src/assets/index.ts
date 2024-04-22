698ea22c6c92828d4b090b22715cbb06755f55ab
Artur7777777777 <artursky7777@gmail.com>
2024-04-22 18:02:05 +0200

    initial commit

---
+++ src/assets/index.ts
@@ -0,0 +1,70 @@
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import mobile from './mobile.png';
+import web from './web.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import css from './tech/css.png';
+import docker from './tech/docker.png';
+import figma from './tech/figma.png';
+import git from './tech/git.png';
+import html from './tech/html.png';
+import javascript from './tech/javascript.png';
+import mongodb from './tech/mongodb.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import angular from './tech/Angular.png';
+import vue from './tech/Vue.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import typescript from './tech/typescript.png';
+import threejs from './tech/threejs.svg';
+
+import it_global_comsulting from './company/IT_Global_Consulting.jpg';
+import software_mind from './company/software_mind_logo.jpg';
+import similarweb from './company/similarweb_logo.jpg';
+
+import memorialkepper from './memorialkeeper.png';
+import petpals from './petpals.png';
+import huzatech from './huzatech.png';
+import littlespoon from './little_spoon.png';
+import mercari from './mercari.png';
+import weenfr from './ween.png';
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  it_global_comsulting,
+  similarweb,
+  software_mind,
+  angular,
+  vue,
+  memorialkepper,
+  petpals,
+  huzatech,
+  littlespoon,
+  mercari,
+  weenfr,
+};
